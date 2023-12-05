24f536c9fc24ca9214582790b620e1e8370df9d9
Diego Fontenelle <diegolimafontenelle@gmail.com>
2023-12-05 16:07:57 -0300

    add day-2 part-2

--- day-2/main.mjs
+++ day-2/main.mjs
@@ -1,44 +1,83 @@
 import fs from 'fs'
 
-function validGame(game) {
+function getCubesCountForColor(color, round) {
+    const digitsPattern = "\\d{1,2}"
+    const digitsRegex = new RegExp(digitsPattern, 'g')
+    const colorRegex = color => new RegExp(`${digitsPattern}\\s\\b${color}\\b`, 'g')
+
+    const cubesByColor = round.match(colorRegex(color))
+    const count = cubesByColor !== null ? cubesByColor.join('').match(digitsRegex)[0] : 0
+
+    return parseInt(count)
+}
+
+function getGames() {
+    const data = fs.readFileSync('./input', 'utf-8')
+    const games = data.split('\n')
+
+    return games
+}
+
+function isValidGame(game) {
     const cubes = {
         red: 12,
         green: 13,
         blue: 14
     }
 
-    const digitsPattern = "\\d{1,2}"
-    const digitsRegex = new RegExp(digitsPattern, 'g')
-    const colorRegex = color => new RegExp(`${digitsPattern}\\s\\b${color}\\b`, 'g')
-
     const rounds = game.split('; ')
 
     for (let round of rounds) {
-        let reds = round.match(colorRegex('red'))
-        let greens = round.match(colorRegex('green'))
-        let blues = round.match(colorRegex('blue'))
+        const redCubes = getCubesCountForColor('red', round)
+        const greenCubes = getCubesCountForColor('green', round)
+        const blueCubes = getCubesCountForColor('blue', round)
 
-        reds = reds !== null ? reds.join('').match(digitsRegex)[0]: '0'
-        greens = greens !== null ? greens.join('').match(digitsRegex)[0] : '0'
-        blues = blues !== null ? blues.join('').match(digitsRegex)[0] : '0'
-
-        if (parseInt(reds) > cubes.red || parseInt(greens) > cubes.green || parseInt(blues) > cubes.blue) {
-            return false 
+        if (redCubes > cubes.red || greenCubes > cubes.green || blueCubes > cubes.blue) {
+            return false
         }
     }
-    return true 
+    return true
+}
+
+
+function partTwo() {
+    const games = getGames()
+    const minCubes = {
+        red: 0,
+        green: 0,
+        blue: 0
+    }
+    let sum = 0
+
+    games.forEach(game => {
+        game.split('; ').forEach(round => {
+            const redCubes = getCubesCountForColor('red', round)
+            const greenCubes = getCubesCountForColor('green', round)
+            const blueCubes = getCubesCountForColor('blue', round)
+
+            minCubes.red = minCubes.red < redCubes ? redCubes : minCubes.red
+            minCubes.green = minCubes.green < greenCubes ? greenCubes : minCubes.green
+            minCubes.blue = minCubes.blue < blueCubes ? blueCubes : minCubes.blue
+        })
+
+        sum += minCubes.red * minCubes.green * minCubes.blue
+        minCubes.red = 0
+        minCubes.green = 0
+        minCubes.blue = 0
+    })
+
+    console.log(sum)
 }
 
 function partOne() {
-    const data = fs.readFileSync('./input', 'utf-8')
-    const games = data.split('\n')
+    const games = getGames()
     let validGames = 0
 
     for (let index = 0; index < games.length; index++) {
         const game = games[index]
 
         if (game) {
-            if (validGame(game)) {
+            if (isValidGame(game)) {
                 validGames += (index + 1)
             }
         }
@@ -48,3 +87,4 @@ function partOne() {
 }
 
 partOne()
+partTwo()
